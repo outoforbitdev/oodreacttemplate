ded05062bacfe9a21358c9268f3eb67773cd646d
Jay Mirecki <jarett.mirecki@gmail.com>
2023-03-13 10:18:53 -0500

    feat(home): use dark mode theme and center text

--- oodreacttemplate/ClientApp/src/App.tsx
+++ oodreacttemplate/ClientApp/src/App.tsx
@@ -1,15 +1,18 @@
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-import { Home } from './components/Home';
+import React, { Component } from "react";
+import { Route } from "react-router-dom";
+import { Home } from "./components/Home";
 
-import './custom.css'
+import "./custom.css"
+import { setTheme, Themes } from "./lib/oodreactts/v1/utils/webpage";
+import "./lib/oodreactts/src/v1/styles/Themes.css"
 
 export default class App extends Component {
   static displayName = App.name;
 
   render () {
+    setTheme(Themes.Dark)
     return (
-      <Route exact path='/' component={Home} />
+      <Route exact path="/" component={Home} />
     );
   }
 }
--- oodreacttemplate/ClientApp/src/components/Home.tsx
+++ oodreacttemplate/ClientApp/src/components/Home.tsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 
+import "../lib/oodreactts/v1/styles/OODCore.css"
+
 export class Home extends Component {
   static displayName = Home.name;
 
   render () {
     return (
-      <div>
+      <div className={"OODCoreStyleCenteredContent"} style={{height: "100vh"}}>
         <h1>Out of Orbit Development</h1>
       </div>
     );
